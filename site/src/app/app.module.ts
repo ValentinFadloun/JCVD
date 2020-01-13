d568bfd61cf3fce40d3d070b8d353987f9d78625
Valentin Fadloun <v.fadloun@gmail.com>
2020-01-13 10:53:13 +0100

    Ajout route
    Ajout de route et composant inscription connexion (vide -- en cours)

--- site/src/app/app.module.ts
+++ site/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ConnexionInscriptionComponent } from './connexion-inscription/connexion-inscription.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ConnexionInscriptionComponent
   ],
   imports: [
     BrowserModule,
---
+++ site/src/app/connexion-inscription/connexion-inscription.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-connexion-inscription',
+  templateUrl: './connexion-inscription.component.html',
+  styleUrls: ['./connexion-inscription.component.scss']
+})
+export class ConnexionInscriptionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
