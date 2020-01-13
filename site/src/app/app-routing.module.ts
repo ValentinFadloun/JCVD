62a464dbcc72c38ea44970f90ffcd13189b6960a
Valentin Fadloun <v.fadloun@gmail.com>
2020-01-13 13:53:39 +0100

    Pqge connexion html css

--- site/src/app/app-routing.module.ts
+++ site/src/app/app-routing.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';/*
 import { PageHome } from './page_home.component';
-import { PageSearch } from './page_search.component';
+import { PageSearch } from './page_search.component';*/
 
 const routes: Routes = [{
   path: '', redirectTo: 'home', pathMatch: 'full'
-},{
+}/*,{
   path: 'home', component:PageHome
 },{
   path: 'search', component:PageSearch
-}];
+}*/];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
